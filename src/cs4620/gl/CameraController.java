31a3f962b5b935ad9e677f37abb30e760e614edd
ishablella <iWang.wsi@gmail.com>
2015-12-11 06:00:42 -0500

    added camera path and traversal speedup feature

--- src/cs4620/gl/CameraController.java
+++ src/cs4620/gl/CameraController.java
@@ -1,5 +1,7 @@
 package cs4620.gl;
 
+import java.util.ArrayList;
+
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 
@@ -7,10 +9,12 @@ import cs4620.common.Scene;
 import cs4620.common.SceneObject;
 import cs4620.common.event.SceneTransformationEvent;
 import egl.math.Matrix4;
+import egl.math.Vector2;
 import egl.math.Vector3;
 import egl.math.Vector3d;
 import cs4620.common.Projectile;
 import cs4620.common.SceneLight;
+import cs4620.splines.CatmullRom;
 public class CameraController {
 	protected final Scene scene;
 	public RenderCamera camera;
@@ -24,9 +28,16 @@ public class CameraController {
 	protected int centerMouseY = 300;
 	protected int minMove = 1;
 	protected int maxMove = 200;
+
 	protected Projectile fling = new Projectile("Lamp1");
 
 	protected boolean orbitMode = false;
+
+	private float step = .1f;
+	private float speed = 1;
+	private int pathno = -1;
+	private Vector2 start = new Vector2(8,7);
+	private Vector2 pos = start;
 
 	public CameraController(Scene s, RenderEnvironment re, RenderCamera c) {
 		scene = s;
@@ -51,66 +62,43 @@ public class CameraController {
 		Vector3 motion = new Vector3();
 		Vector3 rotation = new Vector3();
 
-		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-			motion.add(0, 0, -1);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-			motion.add(0, 0, 1);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-			motion.add(-1, 0, 0);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-			motion.add(1, 0, 0);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
-			motion.add(0, -1, 0);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
-			motion.add(0, 1, 0);
-		}
+//		if (Keyboard.isKeyDown(Keyboard.KEY_W)) { motion.add(0, 0, -1); }
+//		if (Keyboard.isKeyDown(Keyboard.KEY_S)) { motion.add(0, 0, 1); }
+//		if (Keyboard.isKeyDown(Keyboard.KEY_A)) { motion.add(-1, 0, 0); }
+//		if (Keyboard.isKeyDown(Keyboard.KEY_D)) { motion.add(1, 0, 0); }
 
-		if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
-			rotation.add(0, 0, -1);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
+		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) { rotation.add(0.5f, 0, 0); }
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) { rotation.add(-0.5f, 0, 0); }
+		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) { rotation.add(0, -0.5f, 0); }
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) { rotation.add(0, 0.5f, 0); }
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE) && pathno < 0) { pathno++; }
+		else if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) { speed = 5; }
+		else { speed = 1; }
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_Q)) { 
 			fling.activate(camera.mWorldTransform.getZ().clone().normalize().negate());
 		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
-			rotation.add(0.5f, 0, 0);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
-			rotation.add(-0.5f, 0, 0);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
-			rotation.add(0, -0.5f, 0);
-		}
-		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
-			rotation.add(0, 0.5f, 0);
-		}
-		//
-		// if(Keyboard.isKeyDown(Keyboard.KEY_O)) { orbitMode = true; }
-		// if(Keyboard.isKeyDown(Keyboard.KEY_F)) { orbitMode = false; }
-		boolean thisFrameButtonDown = Mouse.isButtonDown(0) && !(Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) || Keyboard.isKeyDown(Keyboard.KEY_RCONTROL));
-		
+
+		boolean thisFrameButtonDown = Mouse.isButtonDown(0) && !(Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) || 
+				Keyboard.isKeyDown(Keyboard.KEY_RCONTROL));
+
 		int thisMouseX = Mouse.getX(), thisMouseY = Mouse.getY();
 		if (thisFrameButtonDown && prevFrameButtonDown) {
-//			hasClicked = !hasClicked;
-//		}
-//		if (hasClicked){
 			rotation.add(0, -0.1f * (thisMouseX - prevMouseX), 0);
 			rotation.add(0.1f * (thisMouseY - prevMouseY), 0, 0);
 		}
 		prevFrameButtonDown = thisFrameButtonDown;
 		prevMouseX = thisMouseX;
 		prevMouseY = thisMouseY;
+
 //		if (!mouseCentered) {
 //			Mouse.setCursorPosition(centerMouseX, centerMouseY);
 //			mouseCentered = true;
 //		} else {
 //			int moveX = Mouse.getX() - centerMouseX, moveY = (Mouse.getY() - centerMouseY);
 //			int move = Math.abs(moveX) > Math.abs(moveY) ? 0 : 1; // 0 is X, 1
-//																	// is Y
+//			// is Y
 //			switch (move) {
 //			case 0:
 //				if (Math.abs(moveX) < maxMove && Math.abs(moveX) > minMove) {
@@ -118,46 +106,86 @@ public class CameraController {
 //					// rotation.add(0, -1, 0);
 //					// } else {
 //					// rotation.add(0, 1, 0);
-//					// }
-//					int sign = moveX / Math.abs(moveX);
-//					for (int i = 0; i < Math.abs(moveY); i++) {
-//						rotation.add(0,0.1f*-sign, 0);
-//					}
-//					//Mouse.setCursorPosition(centerMouseX, centerMouseY);
-//					mouseCentered = false;
 //				}
-//				break;
-//			case 1:
-//				if (Math.abs(moveY) < maxMove && Math.abs(moveY) > minMove) {
-//					// if (moveY > 0) {
-//					// rotation.add(1, 0, 0);
-//					// } else {
-//					// rotation.add(-1, 0, 0);
-//					// }
-//					int sign = moveY / Math.abs(moveY);
-//					for (int i = 0; i < Math.abs(moveY); i++) {
-//						rotation.add(0.1f *sign, 0, 0);
-//					}
-//					//Mouse.setCursorPosition(centerMouseX, centerMouseY);
-//					mouseCentered = false;
+//				int sign = moveX / Math.abs(moveX);
+//				for (int i = 0; i < Math.abs(moveY); i++) {
+//					rotation.add(0,0.1f*-sign, 0);
 //				}
-//				break;
+//				//Mouse.setCursorPosition(centerMouseX, centerMouseY);
+//				mouseCentered = false;
 //			}
+//			break;
+//		case 1:
+//			if (Math.abs(moveY) < maxMove && Math.abs(moveY) > minMove) {
+//				// if (moveY > 0) {
+//				// rotation.add(1, 0, 0);
+//				// } else {
+//				// rotation.add(-1, 0, 0);
+//				// }
+//				int sign = moveY / Math.abs(moveY);
+//				for (int i = 0; i < Math.abs(moveY); i++) {
+//					rotation.add(0.1f *sign, 0, 0);
+//				}
+//				//Mouse.setCursorPosition(centerMouseX, centerMouseY);
+//				mouseCentered = false;
+//			}
+//			break;
+//		}
 
-			RenderObject parent = rEnv.findObject(scene.objects.get(camera.sceneObject.parent));
-			Matrix4 pMat = parent == null ? new Matrix4() : parent.mWorldTransform;
-			if (motion.lenSq() > 0.01) {
-				motion.normalize();
-				motion.mul(5 * (float) et);
-				translate(pMat, camera.sceneObject.transformation, motion);
+		switch (pathno) {
+		case 0:
+			if (pos.x <= -6) { pathno++; } 
+			else { motion.add(-step, 0, 0); }
+			break;
+		case 1:
+			if (pos.y <= -7) { pathno++; } 
+			else { motion.add(0, 0, -step); }
+			break;
+		case 2:
+			if (pos.y >= -3 || pos.x >= -4) { pathno++; } 
+			else { 
+				motion.add(step, 0, 0);
+				motion.add(0, 0, step);
 			}
-			if (rotation.lenSq() > 0.01) {
-				rotation.mul((float) (100.0 * et));
-				rotate(pMat, camera.sceneObject.transformation, rotation);
+			break;
+		case 3:
+			if (pos.y <= -7) { pathno++; }
+			else { 
+				motion.add(step, 0, 0);
+				motion.add(0, 0, -step);
 			}
-			shoot(fling);
-			scene.sendEvent(new SceneTransformationEvent(camera.sceneObject));
-		//}
+			break;
+		case 4:
+			if (pos.x >= 5) { pathno++; } 
+			else { motion.add(step, 0, 0); }
+			break;
+		case 5:
+			if (pos.y >= -1) { pathno++; } 
+			else { motion.add(0, 0, step); }
+			break;
+		case 6:
+			if (pos.x <= 3) { pathno++; } 
+			else { motion.add(-step, 0, 0); }
+			break;
+		case 7:
+			if (pos.y >= 4) { pathno++; } 
+			else { motion.add(0, 0, step); }
+			break;
+		}
+
+		RenderObject parent = rEnv.findObject(scene.objects.get(camera.sceneObject.parent));
+		Matrix4 pMat = parent == null ? new Matrix4() : parent.mWorldTransform;
+		if (motion.lenSq() > 0.01) {
+			motion.normalize();
+			motion.mul(speed * (float) et);
+			translate(pMat, camera.sceneObject.transformation, motion);
+		}
+		if (rotation.lenSq() > 0.01) {
+			rotation.mul((float) (100.0 * et));
+			rotate(pMat, camera.sceneObject.transformation, rotation);
+		}
+		shoot(fling);
+		scene.sendEvent(new SceneTransformationEvent(camera.sceneObject));
 	}
 
 	/**
@@ -182,27 +210,11 @@ public class CameraController {
 		// TODO#A3 SOLUTION START
 
 		rotation = (rotation).mul((float) (Math.PI / 180.0));
-		// put bounds on how far the camera rotates
-		// Matrix4 mRot = new Matrix4();
-		// if (Math.abs(mainDirection.normalize().y -
-		// camera.mViewProjection.getZ().normalize().y) <= .5){
-		// mRot = Matrix4.createRotationX(rotation.x);
-		// System.out.println(mainDirection.normalize() +" "+
-		// camera.mViewProjection.getZ().normalize());
-		// }
 		Matrix4 mRotx = new Matrix4();
 		mRotx = Matrix4.createRotationX(rotation.x);
 		Matrix4 mRoty = Matrix4.createRotationY(rotation.y);
 		mRoty.mulBefore(Matrix4.createTranslation(camera.mWorldTransform.getTrans().negate()));
 		mRoty.mulAfter(Matrix4.createTranslation(camera.mWorldTransform.getTrans()));
-		// mRot.mulAfter(Matrix4.createRotationZ(rotation.z));
-		// if (orbitMode) {
-		// Vector3 rotCenter = new Vector3(0,0,0);
-		// transformation.clone().invert().mulPos(rotCenter);
-		// parentWorld.clone().invert().mulPos(rotCenter);
-		// mRot.mulBefore(Matrix4.createTranslation(rotCenter.clone().negate()));
-		// mRot.mulAfter(Matrix4.createTranslation(rotCenter));
-		// }
 
 		transformation.mulBefore(mRotx);
 
@@ -229,7 +241,10 @@ public class CameraController {
 
 		Matrix4 mTrans = Matrix4.createTranslation(motion);
 
-		transformation.mulBefore(mTrans);
+		transformation.mulAfter(mTrans);
+
+		Vector3 pos3 = transformation.getTrans();
+		pos.set(pos3.x, pos3.z);
 
 		// SOLUTION END
 	}
